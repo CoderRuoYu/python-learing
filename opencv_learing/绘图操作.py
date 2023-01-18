dcc6c5cc57ec806af0ba865ffb9e9fc65f221714
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-01-18 11:02:04 +0800

    test_2023_1_18

--- opencv_learing/绘图操作.py
+++ opencv_learing/绘图操作.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2 as cv
 img = np.zeros((512, 512, 3), np.uint8)
 cv.line(img, (0, 0), (511, 511), (255, 0, 0), 2)
-cv.retangle(img,(0,512),(10,10),(255,0,0),1)
+cv.rectangle(img, (0, 0), (60, 40), (0, 255, 0), 1)
+cv.ellipse(img, (256, 256), (100, 50), 90, 0, 360, (255, 255, 255), -1)
 cv.imshow("result", img)
 cv.waitKey(0)
 cv.destroyAllWindows()
